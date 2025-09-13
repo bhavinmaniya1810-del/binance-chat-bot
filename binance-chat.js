2d15a5ba30de476272f6ea28d5ddd16ff625d366
bhavinmaniya1810-del <bhavinmaniya1810@gmail.com>
2025-09-13 15:53:25 +0400

    Update binance-chat.js

--- binance-chat.js
+++ binance-chat.js
@@ -157,7 +157,7 @@ body {font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', sans-
         await page.setViewport({
             width: 1000,
             height: 392,
-            deviceScaleFactor: 3
+            deviceScaleFactor: 2
         });
 
         await page.setContent(html, { waitUntil: 'networkidle0' });
